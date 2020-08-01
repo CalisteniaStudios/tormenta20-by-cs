ae788c7feb1ca7741b19629e0ff524f7e1728ff4
Vizael <paiva.vhp@gmail.com>
2020-08-01 15:28:59 -0300

    .

--- module/tormenta20.js
+++ module/tormenta20.js
@@ -17,7 +17,7 @@ Hooks.once('init', async function() {
    * @type {String}
    */
   CONFIG.Combat.initiative = {
-    formula: "1d20 + @abilities.dex.mod",
+    formula: "1d20 + @pericias.ini.value",
     decimals: 2
   };
 
