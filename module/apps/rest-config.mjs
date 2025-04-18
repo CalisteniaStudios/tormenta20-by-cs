e749d6d938c87108cfbfb0008b6da8080ebf0641
Matheus Clemente <matheus.xb@gmail.com>
2025-04-18 06:49:23 -0300

    DialogV2: RestConfig
    RestConfig atualizado para V2.
    Mudanças no CSS que afetavam janelas de Dialog/Apps

--- module/apps/rest-config.mjs
+++ module/apps/rest-config.mjs
@@ -1,18 +1,7 @@
-export default class RestConfigDialog extends Dialog {
-	constructor(actors, dialogData={}, options={}) {
-		super(dialogData, options);
-		this.options.classes.push(...["tormenta20", "rest-dialog"]);
-
-		/**
-		 * Store a reference to the Actors document being used
-		 * @type {Array} @type {ActorsT20}
-		 */
-		this.actors = actors;
-	}
-
-	/** @override */
-	activateListeners(html) {
-		super.activateListeners(html);
+export default class RestConfigDialog extends foundry.applications.api.DialogV2 {
+	constructor(options={}) {
+		super(options);
+		this.options.classes.push(...["tormenta20"]);
 	}
 
 	static async create(actors) {
@@ -81,46 +70,45 @@ export default class RestConfigDialog extends Dialog {
 		</div>
 		<div class="form-group">
 				<label>PV Extra / Por Nível</label>
-				<input type='number' name='modPV' value='0'>
+				<div class="form-fields"><input type='number' name='modPV' value='0'></div>
 		</div>
 		<div class="form-group">
 				<label>PM Extra / Por Nível</label>
-				<input type='number' name='modPM' value='0'>
+				<div class="form-fields"><input type='number' name='modPM' value='0'></div>
 		</div>
 		<div class="form-group">
 				<label>Cuidados Prolongados</label>
-				<input type='checkbox' name='curaCP' value=1>
+				<div class="form-fields"><input type='checkbox' name='curaCP' value=1></div>
 		</div>
-	<div class="form-group">
+		<div class="form-group">
 				<label>Acompanhamento Médico</label>
-				<input type='checkbox' name='curaAC' value=1>
+				<div class="form-fields"><input type='checkbox' name='curaAC' value=1></div>
 		</div>
 		</form>`;
 
-		return await new Promise((resolve) => {
-			const dlg = new this(actors, {
+		return this.wait({
+			actors,
+			window: {
 				title: "Descanso",
-				content,
-				buttons: {
-					ok: {
-						label: "OK",
-						callback: (html) => {
-							const modQ = parseFloat(html.find("[name=qualidade]")[0].value);
-							const modPV = parseInt(html.find("[name=modPV]")[0].value);
-							const modPM = parseInt(html.find("[name=modPM]")[0].value);
-							const curaCP = html.find("[name=curaCP]")[0].checked;
-							const curaAC = html.find("[name=curaAC]")[0].checked;
-							descanso(actors, modQ, modPV, modPM, curaCP, curaAC);
-
-						}
-					}
-				},
-				default: "",
-				close: () => {}
-			});
-			dlg.options.width = 600;
-			dlg.position.width = 600;
-			dlg.render(true);
+				icon: "fa-solid fa-bed"
+			},
+			content,
+			buttons: [{
+				label: "OK",
+				action: "ok",
+				callback: (event, button, dlg) => {
+					const modQ = parseFloat(dlg.element.querySelector("[name=qualidade]").value);
+					const modPV = parseInt(dlg.element.querySelector("[name=modPV]").value);
+					const modPM = parseInt(dlg.element.querySelector("[name=modPM]").value);
+					const curaCP = dlg.element.querySelector("[name=curaCP]").checked;
+					const curaAC = dlg.element.querySelector("[name=curaAC]").checked;
+					descanso(actors, modQ, modPV, modPM, curaCP, curaAC);
+				}
+			},
+			{
+				label: "Cancel"
+			}],
+			position: { width: 400 }
 		});
 	}
 }
