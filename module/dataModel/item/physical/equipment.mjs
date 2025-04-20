6218e7e733626908587b7954073a379590a7f60f
Matheus Clemente <matheus.xb@gmail.com>
2025-04-20 05:26:22 -0300

    Revert "Equipamento: negativar Penalidade de Armadura"
    This reverts commit 9e673250f34b8add032d0476d78b523069dc333f.

--- module/dataModel/item/physical/equipment.mjs
+++ module/dataModel/item/physical/equipment.mjs
@@ -1,4 +1,3 @@
-import { NegativeNumberField } from "../../helpers.mjs";
 import Tormenta20ItemData from "../item.mjs";
 
 const fields = foundry.data.fields;
@@ -20,7 +19,7 @@ export default class EquipmentData extends Tormenta20ItemData {
 			}),
 			armadura: new fields.SchemaField({
 				maxAtr: new fields.NumberField({ required: true, nullable: false, initial: 0, label: "T20.ItemEquipmentDefenseMaxAbility", hint: "T20.ItemEquipmentDefenseMaxAbilityHint" }),
-				penalidade: new NegativeNumberField({ required: true, nullable: false, initial: 0, label: "T20.ItemEquipmentArmorPenalty", hint: "T20.ItemEquipmentArmorPenaltyHint" }),
+				penalidade: new fields.NumberField({ required: true, nullable: false, initial: 0, label: "T20.ItemEquipmentArmorPenalty", hint: "T20.ItemEquipmentArmorPenaltyHint" }),
 				value: new fields.NumberField({ required: true, nullable: false, initial: 0, label: "T20.ItemEquipmentDefenseValue", hint: "T20.ItemEquipmentDefenseValueHint" })
 			}),
 			tipo: new fields.StringField({ required: true, nullable: false, initial: "leve", label: "T20.ItemType", hint: "T20.ItemTypeHint" })
