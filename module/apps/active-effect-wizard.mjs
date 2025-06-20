3bd6a98a662aa47e76b30be39577fe54eda3fa85
Matheus Clemente <matheus.xb@gmail.com>
2025-06-20 15:40:07 -0300

    Update active-effect-wizard.mjs

--- module/apps/active-effect-wizard.mjs
+++ module/apps/active-effect-wizard.mjs
@@ -240,11 +240,11 @@ export default class ActiveEffectWizard extends HandlebarsApplicationMixin(Appli
 			{
 				label: `${game.i18n.localize("T20.Defense")}: ${game.i18n.localize("T20.Bonus")}`,
 				key: "system.attributes.defesa.bonus"
-			},
-			{
-				label: `${game.i18n.localize("T20.Defense")}: ${game.i18n.localize("T20.Ability")}`,
-				key: "system.attributes.defesa.atributo"
 			}
+			// {
+			// 	label: `${game.i18n.localize("T20.Defense")}: ${game.i18n.localize("T20.Ability")}`,
+			// 	key: "system.attributes.defesa.atributo"
+			// }
 		];
 	}
 
@@ -336,10 +336,10 @@ export default class ActiveEffectWizard extends HandlebarsApplicationMixin(Appli
 
 	#getOtherStatsPresets() {
 		return [
-			{
-				label: game.i18n.localize("T20.Size"),
-				key: "system.tracos.tamanho"
-			},
+			// {
+			// 	label: game.i18n.localize("T20.Size"),
+			// 	key: "system.tracos.tamanho"
+			// },
 			{
 				label: game.i18n.localize("T20.SpellDC"),
 				key: "system.attributes.cd"
