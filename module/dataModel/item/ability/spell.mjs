228ec3cdf71835e3979bc90eb1a370122b35580f
Matheus Clemente <matheus.xb@gmail.com>
2025-07-19 21:04:33 -0300

    Update spell.mjs

--- module/dataModel/item/ability/spell.mjs
+++ module/dataModel/item/ability/spell.mjs
@@ -11,24 +11,29 @@ export default class SpellData extends Tormenta20ItemData {
 			...this.schemaActivation(type),
 			...this.schemaSavingThrow(type),
 			...this.schemaRolls(),
-			circulo: new fields.StringField({
+			circulo: new fields.NumberField({
 				required: true,
 				nullable: false,
-				initial: "1",
+				initial: 1,
+				choices: CONFIG.T20.spellLevels,
 				label: "T20.ItemSpellCircle",
 				hint: "T20.ItemSpellCircleHint"
 			}),
 			escola: new fields.StringField({
 				required: true,
 				nullable: false,
-				initial: "",
+				blank: false,
+				initial: Object.keys(CONFIG.T20.spellSchools)[0],
+				choices: CONFIG.T20.spellSchools,
 				label: "T20.ItemSpellSchool",
 				hint: "T20.ItemSpellSchoolHint"
 			}),
 			tipo: new fields.StringField({
 				required: true,
 				nullable: false,
-				initial: "",
+				blank: false,
+				initial: Object.keys(CONFIG.T20.spellType)[0],
+				choices: CONFIG.T20.spellType,
 				label: "T20.ItemType",
 				hint: "T20.ItemTypeHint"
 			}),
